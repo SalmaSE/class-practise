7ab4bbb7e8f01e1ba6467bc9145e78507e005d5e
Salma <selma.1390@gmail.com>
2024-08-07 21:06:20 -0400

    initial push

---
+++ src/test/Practice.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Practice {
+    public static void main  (String arg[]) {
+        int a = 3;
+        int b = 10;
+        System.out.println("Hi, my name is Salma");
+        System.out.println(a + b);
+
+        System.out.print (b-a + "\n");
+        System.out.printf("hi");
+        System.out.printf("");
+    }}
+
